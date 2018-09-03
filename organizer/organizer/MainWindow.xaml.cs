ffe2eb0b6431a66a3b6fadb9dcfd203dd1cadf74
okiochan <maryalx@mail.ru>
2018-09-03 20:06:19 +0300

    added dialog

--- organizer/organizer/MainWindow.xaml.cs
+++ organizer/organizer/MainWindow.xaml.cs
@@ -19,13 +19,48 @@ namespace organizer
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<TaskFolder> tasks;
-
+        private List<TaskFolder> tasks;
+        private List<System.Windows.Controls.StackPanel> spList;
+        private int butCnt=0;
+        
         public MainWindow()
         {
             InitializeComponent();
+            prepeareData();
         }
 
+        //masha add
+        private void prepSpList() {
+            if (butCnt % 5 == 0) {
+                System.Windows.Controls.StackPanel sp = new StackPanel();
+                sp.Orientation = System.Windows.Controls.Orientation.Horizontal;
+                spList.Add(sp);
+                panelTasks.Children.Add(sp);
+            }
+        }
+
+        //masha add
+        private void prepeareData() {
+            tasks = new List<TaskFolder>();
+            spList = new List<System.Windows.Controls.StackPanel>();
+
+            TaskFolder tf = new TaskFolder();
+            tf.name = "fsdfsdf";
+            tf.status = Status.TODO;
+            tasks.Add(tf); tasks.Add(tf); tasks.Add(tf); tasks.Add(tf); tasks.Add(tf); tasks.Add(tf);
+            //read tasks
+            
+            //display tasks
+            foreach(var t in tasks) {
+                prepSpList();
+                System.Windows.Controls.Button butt = new Button();
+                butt.Content = t.name;
+                butt.Name = "but" + butCnt.ToString();
+                butCnt++;
+                spList[spList.Count - 1].Children.Add(butt);
+            }
+        }
+        
         //masha add
         private void butAddTask_Click(object sender, RoutedEventArgs e) {
 
@@ -33,9 +68,21 @@ namespace organizer
             DialogAddFolderTask wind = new DialogAddFolderTask();
             if (wind.ShowDialog() == true) {
                 tf.name = wind.getTaskTitle;
+                tf.status = Status.TODO;
+                tasks.Add(tf);
+
+                prepSpList();
+                System.Windows.Controls.Button butt = new Button();
+                butt.Content = tf.name;
+                butt.Name = "but" + butCnt.ToString();
+                butCnt++;
+                spList[spList.Count - 1].Children.Add(butt);
+
             } else {
                 MessageBox.Show("Folder dialog not opened =(");
             }
+
+            mainWindow.UpdateLayout();
 
         }
 
